637096b84abfb41a1b5e0f8245a50ec739320379
Ganesh12062002 <ganeshparakhe0@gmail.com>
2024-10-14 11:45:45 +0530

    Add files via upload

---
+++ spuer_method.py
@@ -0,0 +1,14 @@
+class college:
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name
+
+class student(college):
+    def __init__(self,id,name,city):
+        super().__init__(id,name)
+        self.city=city
+
+obj =student(101,"om","pune")
+print(obj.id)
+print(obj.name)
+print(obj.city)
